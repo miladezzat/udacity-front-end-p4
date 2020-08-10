0f7571d3112bf62f10fc57843bc525d05676f308
Milad E. Fahmy <miladezzat.f@gmail.com>
2020-08-10 20:09:17 +0200

    port

--- src/server/index.js
+++ src/server/index.js
@@ -21,12 +21,13 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, 'dist')));
 
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname,'../../dist/index.html'));    
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../dist/index.html'));
 })
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!')
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Example app listening on port :' + port)
 })
 
 
